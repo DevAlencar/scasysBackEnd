df4a70984ed79dad010acd380c4c1c79b178ce8a
DevAlencar <arthur.alencartrindade@gmail.com>
2024-02-09 11:25:50 -0300

    .

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -105,8 +105,14 @@ module.exports = {
 
     async delete(req, res) {
         const id = req.params.id;
+        const user = await User.findByIdAndDelete({ _id: id });
+
+        //validando se o usuário foi encontrado
+        if (!user) {
+            return res.status(404).json({ msg: "Usuário não encontrado" });
+        }
+
         try {
-            await User.findByIdAndDelete({ _id: id });
             return res
                 .status(200)
                 .json({ msg: "Usuário deletado com sucesso" });
