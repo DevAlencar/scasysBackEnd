dac998ac5544357ed21acb0af287f8e86407d1d4
DevAlencar <arthur.alencartrindade@gmail.com>
2024-02-20 20:35:16 -0300

    exp model

--- src/models/ExpData.js
+++ src/models/ExpData.js
@@ -29,7 +29,6 @@ const ExpDataSchema = new mongoose.Schema(
                     type: Number,
                     required: true,
                 },
-                //TODO:perguntar sobre etapa procedimental
                 especifity: {
                     type: String,
                     required: true,
@@ -42,17 +41,16 @@ const ExpDataSchema = new mongoose.Schema(
                     type: String,
                     required: true,
                 },
-                //TODO:perguntar sobre página 4
                 quantity: {
                     type: Array,
                     value: {
                         type: String,
                         required: true,
                     },
-                    unit: {
-                        type: String,
-                        required: true,
-                    },
+                },
+                unit: {
+                    type: String,
+                    required: true,
                 },
                 observation: {
                     type: String,
@@ -63,7 +61,7 @@ const ExpDataSchema = new mongoose.Schema(
             mmr: {
                 type: String,
                 required: true,
-            }, //TODO:Verificar campos de seleção
+            },
             mtdr: {
                 type: String,
                 required: true,
@@ -85,14 +83,307 @@ const ExpDataSchema = new mongoose.Schema(
                 required: true,
             },
         },
-        security_stage: {
+        security_stage_one: {
+            exposition: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                conc_tox_lim: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                exp_time: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+            acumulation: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                fa: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit_esp: {
+                        type: String,
+                        required: true,
+                    },
+                    unit_met: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+        },
+        cm_stage: {
+            type: Object,
             quim_comp: {
                 type: String,
                 required: true,
             },
-            conc_tox_lim: {
+            cancer: {
+                type: Boolean,
+                required: true,
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        security_stage_two: {
+            exp_pot: {
                 type: Object,
-                value: {
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                fe: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+            rgc: {
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                quantity: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                gas_pressure: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                atm_pressure: {
+                    type: Object,
+                    value: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                gas_state: {
+                    type: String,
+                    required: true,
+                },
+                amb_temp: {
+                    type: Number,
+                    required: true,
+                },
+                comp_tem: {
+                    type: Number,
+                    required: true,
+                },
+            },
+            fri: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                inc_risc: {
+                    type: Boolean,
+                    required: true,
+                },
+                epa: {
+                    type: Object,
+                    entalpia: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                    src: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                lie: {
+                    type: Object,
+                    limit: {
+                        type: String,
+                        required: true,
+                    },
+                    src: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                fu_point: {
+                    type: Object,
+                    fulgor: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                    src: {
+                        type: String,
+                        required: true,
+                    },
+                },
+            },
+            fg_factor: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                quantity: {
+                    type: String,
+                    required: true,
+                },
+                unity: {
+                    type: String,
+                    required: true,
+                },
+                vgf: {
+                    type: Object,
+                    qtt: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                atm_pressure: {
+                    type: Object,
+                    qtt: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+            corros_factor: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                corros_tax: {
+                    type: Object,
+                    qtt: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                temperature: {
+                    type: Object,
+                    qtt: {
+                        type: String,
+                        required: true,
+                    },
+                    unit: {
+                        type: String,
+                        required: true,
+                    },
+                },
+                aacaa: {
+                    type: Boolean,
+                    required: true,
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+            corros_factor: {
+                type: Object,
+                quim_comp: {
+                    type: String,
+                    required: true,
+                },
+                number_of_security: {
+                    type: Number,
+                    required: true,
+                },
+            },
+        },
+        ce_stage: {
+            type: Object,
+            cata_ocorren: {
+                type: Boolean,
+                required: true,
+            },
+            dur_time_wo_cata: {
+                type: Object,
+                time: {
                     type: String,
                     required: true,
                 },
@@ -101,20 +392,200 @@ const ExpDataSchema = new mongoose.Schema(
                     required: true,
                 },
             },
-            exp_time: {
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        energy_stage: {
+            type: Object,
+            cmd: {
                 type: Object,
-                value: {
+                quantity: {
                     type: String,
                     required: true,
                 },
                 unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+            process_dur: {
+                type: Object,
+                quantity: {
+                    type: String,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        global_warm: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            co2_mass: {
+                type: String,
+                required: true,
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        oz_dan: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            cfc_mass: {
+                type: String,
+                required: true,
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        pot_foto: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            oz_mass: {
+                type: String,
+                required: true,
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        pot_ac: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            ac_mols: {
+                type: String,
+                required: true,
+            },
+            src: {
+                type: String,
+                required: true,
+            },
+        },
+        water_cons: {
+            type: Object,
+            total_vol: {
+                type: Object,
+                qtt: {
+                    type: String,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+            amb_temperature: {
+                type: Object,
+                qtt: {
+                    type: String,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+            water_temperature: {
+                type: Object,
+                qtt: {
+                    type: String,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+            water_garb_temperature: {
+                type: Object,
+                qtt: {
+                    type: String,
+                    required: true,
+                },
+                unit: {
+                    type: String,
+                    required: true,
+                },
+            },
+        },
+        rad_ion: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            radio: {
+                type: String,
+                required: true,
+            },
+            amb_radio: {
+                type: Object,
+                value: {
                     type: String,
                     required: true,
                 },
                 src: {
                     type: String,
                     required: true,
-                }, //TODO:Verificar sobre as fontes bibliog´raficas
+                },
+            },
+        },
+        rad_ion: {
+            type: Object,
+            quim_comp: {
+                type: String,
+                required: true,
+            },
+            mtcd: {
+                type: Object,
+                value: {
+                    type: String,
+                    required: true,
+                },
+                src: {
+                    type: String,
+                    required: true,
+                },
+            },
+            menup: {
+                type: String,
+                required: true,
+            },
+        },
+        hol_amb: {
+            type: Object,
+            value: {
+                type: String,
+                required: true,
+            },
+            unit: {
+                type: String,
+                required: true,
             },
         },
     },
@@ -122,7 +593,3 @@ const ExpDataSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("exp", ExpDataSchema);
-
-/*TODO:
-    falar com guilherme para modelar os experimentos
-*/
