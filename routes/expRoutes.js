f72e72ed0c07a36e3e886eca9d59f06e70f65cc4
DevAlencar <arthur.alencartrindade@gmail.com>
2024-02-24 18:10:12 -0300

    exp routes

--- routes/expRoutes.js
+++ routes/expRoutes.js
@@ -9,5 +9,14 @@ routes.post("/newExp/:id", ExpController.new_procedure);
 routes.post("/invetoryStage/:id", ExpController.add_inventory_stage);
 routes.post("/ppgrStage/:id", ExpController.add_ppgr_stage);
 routes.post("/securityStageOne/:id", ExpController.add_security_stage_one);
+routes.post("/cmStage/:id", ExpController.add_cm_stage);
+routes.post("/securityStageTwo/:id", ExpController.add_security_stage_two);
+routes.post("/ceStage/:id", ExpController.add_ce_stage);
+routes.post("/energyStage/:id", ExpController.add_energy_stage);
+routes.post("/ambStages/:id", ExpController.add_amb_stages);
+routes.post("/waterCons/:id", ExpController.add_water_cons);
+routes.post("/radIonStage/:id", ExpController.add_rad_ion_stage);
+routes.post("/resorCosumStage/:id", ExpController.add_resor_cosum_stage);
+routes.post("/holAmbStage/:id", ExpController.add_hol_amb_stage);
 
 module.exports = routes;
